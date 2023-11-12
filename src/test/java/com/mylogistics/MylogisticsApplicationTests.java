bd5d62f9daeadea12101f4e46cb0732e00770a76
divyavidya <divyavidya025@gmail.com>
2023-11-12 15:35:56 +0530

    first commit

---
+++ src/test/java/com/mylogistics/MylogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
