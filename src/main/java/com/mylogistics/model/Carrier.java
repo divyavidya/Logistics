6e96425c4568433fe242d8e8e1b7198b500935a5
Karishma Shaik <shaikkarishma4689@gmail.com>
2023-11-13 11:58:32 +0530

    added the model classes

--- src/main/java/com/mylogistics/model/Carrier.java
+++ src/main/java/com/mylogistics/model/Carrier.java
@@ -1,5 +1,66 @@
 package com.mylogistics.model;
 
-public class Carrier {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
+
+@Entity
+public class Carrier {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+	private String name;
+	private String address;
+	private String email;
+	private String contact;
+	@OneToOne
+	private User user;
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	@Override
+	public String toString() {
+		return "Carrier [id=" + id + ", name=" + name + ", address=" + address + ", email=" + email + ", contact="
+				+ contact + ", user=" + user + "]";
+	}
+	
+	
+	
 }
