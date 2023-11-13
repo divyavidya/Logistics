eb73f237d7405ef0c3576ed1d9b134fe39ff4af3
divyavidya <divyavidya025@gmail.com>
2023-11-13 12:08:42 +0530

    route,executive models

--- src/main/java/com/mylogistics/model/Route.java
+++ src/main/java/com/mylogistics/model/Route.java
@@ -1,5 +1,62 @@
 package com.mylogistics.model;
 
-public class Route {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity //<-- this will create table employee in the DB
+public class Route {
+	@Id //<-- pls make id as primary key 
+	@GeneratedValue(strategy = GenerationType.AUTO) //<-- this will make id auto_increment
+	private int id;
+	private String source;
+	private String destination;
+	private double distance;
+	private String noOfDays;
+	private String vehicle;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	public String getNoOfDays() {
+		return noOfDays;
+	}
+	public void setNoOfDays(String noOfDays) {
+		this.noOfDays = noOfDays;
+	}
+	
+	public String getVehicle() {
+		return vehicle;
+	}
+	public void setVehicle(String vehicle) {
+		this.vehicle = vehicle;
+	}
+	@Override
+	public String toString() {
+		return "Route [id=" + id + ", source=" + source + ", destination=" + destination + ", distance=" + distance
+				+ ", noOfDays=" + noOfDays + ", vehicle=" + vehicle + "]";
+	}
+	
 }
+
